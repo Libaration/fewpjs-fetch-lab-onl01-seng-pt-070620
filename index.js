f680f56c767aee03a3b542b895e8a23b17ebc3f1
Cristian Deleon <libaration@gmail.com>
2020-12-28 20:31:06 -0500

    total pages function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
       .then(r => r.json())
-      .then(json => renderBooks(json));
+      .then(json => {
+        renderBooks(json)
+        renderPageCount(json)
+      });
 }
 
 function renderBooks(books) {
@@ -11,6 +14,20 @@ function renderBooks(books) {
     h2.innerHTML = book.name
     main.appendChild(h2)
   })
+}
+
+function renderPageCount(books) {
+  const main = document.querySelector('main')
+  let count = 0
+  books.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `${book.name} has ${book.numberOfPages} pages`
+    count += book.numberOfPages
+    main.appendChild(h2)
+  })
+  const h3 = document.createElement('h3')
+  h3.innerHTML = `That's a grand total of ${count} pages!`
+  main.appendChild(h3)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
